8c1fcee83726b8bbd7b29bf4cf916230967861e0
JovannyEspinal <espinaljovanny@icloud.com>
2015-10-20 18:25:07 -0400

    Added Recipe TVC

---
+++ FoodbLog/FoodbLog/RecipeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecipeTableViewController.h
+//  FoodbLog
+//
+//  Created by Jovanny Espinal on 10/20/15.
+//  Copyright © 2015 Ayuna Vogel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeTableViewController : UITableViewController
+
+@end
